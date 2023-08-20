f5f54f17d8dbb92acc899e5f0071156dfc43da35
mg3994 <126542773+mg3994@users.noreply.github.com>
2023-08-20 12:43:24 +0530

    Update path.php

--- larave_mysql_datetime/path.php
+++ larave_mysql_datetime/path.php
@@ -1,37 +1,61 @@
-/**
+// /**
+// // <?php
+// // require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// // $path = [
+// //     "path" => base_path('.env');
+// // ];
+
+// // return $path["path"];
+// // ?>
+
+// */
+
 // <?php
-// require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// // Include Laravel's autoloader
+// require __DIR__.'/vendor/autoload.php';
+
+// /**
+//  * This script retrieves the absolute path to the .env file.
+//  *
+//  * @return string The absolute path to the .env file.
+//  */
+// function getEnvFilePath(): string {
+//     // Get the path to the .env file
+//     $envFilePath = base_path('.env');
+
+//     return $envFilePath;
+// }
+
+// // Define an associative array to hold the path information
 // $path = [
-//     "path" => base_path('.env');
+//     "path" => getEnvFilePath() // Call the function to get the .env file path
 // ];
 
+// // Return the .env file path
 // return $path["path"];
 // ?>
 
-*/
+
 
 <?php
-// Include Laravel's autoloader
-require __DIR__.'/vendor/autoload.php';
-
 /**
  * This script retrieves the absolute path to the .env file.
  *
  * @return string The absolute path to the .env file.
  */
 function getEnvFilePath(): string {
-    // Get the path to the .env file
-    $envFilePath = base_path('.env');
+    // Get the directory of the current script
+    $scriptDirectory = __DIR__;
+
+    // Construct the absolute path to the .env file
+    $envFilePath = $scriptDirectory . '/.env';
 
     return $envFilePath;
 }
 
-// Define an associative array to hold the path information
-$path = [
-    "path" => getEnvFilePath() // Call the function to get the .env file path
-];
+// Call the function to get the .env file path
+$path = getEnvFilePath();
 
 // Return the .env file path
-return $path["path"];
+echo $path;
 ?>
-
