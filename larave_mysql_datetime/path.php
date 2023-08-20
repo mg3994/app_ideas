003a214bbad41195186a51ca85ceefca31c16e93
mg3994 <126542773+mg3994@users.noreply.github.com>
2023-08-20 12:39:59 +0530

    Update path.php

--- larave_mysql_datetime/path.php
+++ larave_mysql_datetime/path.php
@@ -1,8 +1,37 @@
+/**
+// <?php
+// require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// $path = [
+//     "path" => base_path('.env');
+// ];
+
+// return $path["path"];
+// ?>
+
+*/
+
 <?php
-require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// Include Laravel's autoloader
+require __DIR__.'/vendor/autoload.php';
+
+/**
+ * This script retrieves the absolute path to the .env file.
+ *
+ * @return string The absolute path to the .env file.
+ */
+function getEnvFilePath(): string {
+    // Get the path to the .env file
+    $envFilePath = base_path('.env');
+
+    return $envFilePath;
+}
+
+// Define an associative array to hold the path information
 $path = [
-    "path" => base_path('.env');
+    "path" => getEnvFilePath() // Call the function to get the .env file path
 ];
 
+// Return the .env file path
 return $path["path"];
 ?>
+
