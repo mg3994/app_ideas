a93c3eae47dfc98b72456b3480b6ee5686ccc7c7
mg3994 <126542773+mg3994@users.noreply.github.com>
2023-08-20 12:35:09 +0530

    Update program.php

--- larave_mysql_datetime/program.php
+++ larave_mysql_datetime/program.php
@@ -1,3 +1,5 @@
+/*
+
 // // <?php
 // // $appTimezone = "Asia/Calcutta";
 // // $dbTimezone = "+05:30";
@@ -11,6 +13,10 @@
 
 // // echo "Configuration file generated successfully.\n";
 // // ?>
+
+
+
+
 
 
 // <?php
@@ -31,23 +37,61 @@
 // }
 // ?>
 
+// <?php
+// require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// $appTimezone = "Asia/Calcutta";
+// $dbTimezone = "+05:30";
+
+//$envFilePath = base_path('.env');
+
+//if (file_exists($envFilePath)) {
+  //  $content = file_get_contents($envFilePath);
+    //$content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
+    //$content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
+
+    //file_put_contents($envFilePath, $content);
+    //echo "Configuration values updated in .env file.\n";
+//} else {
+  //  echo "Error: .env file not found.\n";
+//}
+//?> 
+*/
 
 <?php
-require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// Include Laravel's autoloader
+require __DIR__.'/vendor/autoload.php';
+
+// Define the timezones you want to set
 $appTimezone = "Asia/Calcutta";
 $dbTimezone = "+05:30";
 
+// Get the path to the .env file
 $envFilePath = base_path('.env');
 
+// Check if the .env file exists
 if (file_exists($envFilePath)) {
+    // Read the content of the .env file
     $content = file_get_contents($envFilePath);
+
+    // Replace the existing APP_TIMEZONE value with the new one
     $content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
+
+    // Replace the existing DB_TIMEZONE value with the new one
     $content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
 
+    // Write the modified content back to the .env file
     file_put_contents($envFilePath, $content);
+
+    // Display a success message
     echo "Configuration values updated in .env file.\n";
 } else {
+    // Display an error message if the .env file is not found
     echo "Error: .env file not found.\n";
 }
 ?>
+
+
+
+
+
 
