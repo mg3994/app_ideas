5a118edd2ca2fe2b6efcf23ba8b6013b0320ff76
mg3994 <126542773+mg3994@users.noreply.github.com>
2023-08-20 13:25:32 +0530

    Update program.php

--- larave_mysql_datetime/program.php
+++ larave_mysql_datetime/program.php
@@ -1,103 +1,4 @@
-// /**
-
-// // // <?php
-// // // $appTimezone = "Asia/Calcutta";
-// // // $dbTimezone = "+05:30";
-
-// // // $configContent = "APP_TIMEZONE={$appTimezone}\n";
-// // // $configContent .= "DB_TIMEZONE={$dbTimezone}";
-
-// // // $file = fopen('.env', 'w');
-// // // fwrite($file, $configContent);
-// // // fclose($file);
-
-// // // echo "Configuration file generated successfully.\n";
-// // // ?>
-
-
-
-
-
-
-// // <?php
-// // $appTimezone = "Asia/Calcutta";
-// // $dbTimezone = "+05:30";
-
-// // $envFilePath = '.env';
-
-// // if (file_exists($envFilePath)) {
-// //     $content = file_get_contents($envFilePath);
-// //     $content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
-// //     $content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
-
-// //     file_put_contents($envFilePath, $content);
-// //     echo "Configuration values updated in .env file.\n";
-// // } else {
-// //     echo "Error: .env file not found.\n";
-// // }
-// // ?>
-
-// // <?php
-// // require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
-// // $appTimezone = "Asia/Calcutta";
-// // $dbTimezone = "+05:30";
-
-// //$envFilePath = base_path('.env');
-
-// //if (file_exists($envFilePath)) {
-//   //  $content = file_get_contents($envFilePath);
-//     //$content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
-//     //$content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
-
-//     //file_put_contents($envFilePath, $content);
-//     //echo "Configuration values updated in .env file.\n";
-// //} else {
-//   //  echo "Error: .env file not found.\n";
-// //}
-// //?> 
-// */
-
-// <?php
-// // Include Laravel's autoloader
-// require __DIR__.'/vendor/autoload.php';
-
-// // Define the timezones you want to set
-// $appTimezone = "Asia/Calcutta";
-// $dbTimezone = "+05:30";
-
-// // Get the path to the .env file
-// $envFilePath = base_path('.env');
-
-// // Check if the .env file exists
-// if (file_exists($envFilePath)) {
-//     // Read the content of the .env file
-//     $content = file_get_contents($envFilePath);
-
-//     // Replace the existing APP_TIMEZONE value with the new one
-//     $content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
-
-//     // Replace the existing DB_TIMEZONE value with the new one
-//     $content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
-
-//     // Write the modified content back to the .env file
-//     file_put_contents($envFilePath, $content);
-
-//     // Display a success message
-//     echo "Configuration values updated in .env file.\n";
-// } else {
-//     // Display an error message if the .env file is not found
-//     echo "Error: .env file not found.\n";
-// }
-// ?>
-
-
-
-
-
 <?php
-// Include Laravel's autoloader
-require __DIR__.'/vendor/autoload.php';
-
 /**
  * This script retrieves the absolute path to the .env file.
  *
@@ -115,6 +16,7 @@ function getEnvFilePath(): string {
 
 // Call the function to get the .env file path
 $envFilePath = getEnvFilePath();
+echo $envFilePath . "<br>";
 
 // Define the timezones you want to set
 $appTimezone = "Asia/Calcutta";
@@ -123,16 +25,20 @@ $dbTimezone = "+05:30";
 // Check if the .env file exists
 if (file_exists($envFilePath)) {
     // Read the content of the .env file
-    $content = file_get_contents($envFilePath);
+    $lines = file($envFilePath);
 
-    // Replace the existing APP_TIMEZONE value with the new one
-    $content = preg_replace('/\bAPP_TIMEZONE=.*\b/', "APP_TIMEZONE={$appTimezone}", $content);
-
-    // Replace the existing DB_TIMEZONE value with the new one
-    $content = preg_replace('/\bDB_TIMEZONE=.*\b/', "DB_TIMEZONE={$dbTimezone}", $content);
+    // Modify the content in memory
+    foreach ($lines as &$line) {
+        if (preg_match('/^\s*APP_TIMEZONE\s*=\s*(.*)$/i', $line, $matches)) {
+            $line = "APP_TIMEZONE = '{$appTimezone}'\n";
+        }
+        if (preg_match('/^\s*DB_TIMEZONE\s*=\s*(.*)$/i', $line, $matches)) {
+            $line = "DB_TIMEZONE = '{$dbTimezone}'\n";
+        }
+    }
 
     // Write the modified content back to the .env file
-    file_put_contents($envFilePath, $content);
+    file_put_contents($envFilePath, implode('', $lines));
 
     // Display a success message
     echo "Configuration values updated in .env file.\n";
@@ -141,10 +47,3 @@ if (file_exists($envFilePath)) {
     echo "Error: .env file not found.\n";
 }
 ?>
-
-
-
-
-
-
-
