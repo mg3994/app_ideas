8a681d46a76e2fc0385507dd1548fdb80668d5d5
mg3994 <126542773+mg3994@users.noreply.github.com>
2023-08-22 22:08:48 +0530

    Update path.php

--- larave_mysql_datetime/path.php
+++ larave_mysql_datetime/path.php
@@ -1,39 +1,66 @@
-// /**
+// // /**
+// // // <?php
+// // // require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// // // $path = [
+// // //     "path" => base_path('.env');
+// // // ];
+
+// // // return $path["path"];
+// // // ?>
+
+// // */
+
 // // <?php
-// // require __DIR__.'/vendor/autoload.php'; // Include Laravel's autoloader
+// // // Include Laravel's autoloader
+// // require __DIR__.'/vendor/autoload.php';
+
+// // /**
+// //  * This script retrieves the absolute path to the .env file.
+// //  *
+// //  * @return string The absolute path to the .env file.
+// //  */
+// // function getEnvFilePath(): string {
+// //     // Get the path to the .env file
+// //     $envFilePath = base_path('.env');
+
+// //     return $envFilePath;
+// // }
+
+// // // Define an associative array to hold the path information
 // // $path = [
-// //     "path" => base_path('.env');
+// //     "path" => getEnvFilePath() // Call the function to get the .env file path
 // // ];
 
+// // // Return the .env file path
 // // return $path["path"];
 // // ?>
 
-// */
+
 
 // <?php
-// // Include Laravel's autoloader
-// require __DIR__.'/vendor/autoload.php';
-
 // /**
 //  * This script retrieves the absolute path to the .env file.
 //  *
 //  * @return string The absolute path to the .env file.
 //  */
-// function getEnvFilePath(): string {
-//     // Get the path to the .env file
-//     $envFilePath = base_path('.env');
+// function getEnvFilePath() {
+//     // Get the directory of the current script
+//     $scriptDirectory = __DIR__;
 
-//     return $envFilePath;
+//     // Construct the absolute path to the .env file
+//     $envFilePath = $scriptDirectory . '/.env';
+
+//     return [$envFilePath];
 // }
 
-// // Define an associative array to hold the path information
-// $path = [
-//     "path" => getEnvFilePath() // Call the function to get the .env file path
-// ];
+// // Call the function to get the .env file path
+// $path = getEnvFilePath();
 
 // // Return the .env file path
-// return $path["path"];
+// echo $path;
 // ?>
+
+
 
 
 
@@ -41,21 +68,25 @@
 /**
  * This script retrieves the absolute path to the .env file.
  *
- * @return string The absolute path to the .env file.
+ * @return array An array containing the absolute path to the .env file.
  */
-function getEnvFilePath(): string {
+function getEnvFilePath(): array {
     // Get the directory of the current script
     $scriptDirectory = __DIR__;
 
     // Construct the absolute path to the .env file
-    $envFilePath = $scriptDirectory . '/.env';
+    $envFilePath = $scriptDirectory . '\.env';
 
-    return $envFilePath;
+    return [$envFilePath];
 }
 
 // Call the function to get the .env file path
 $path = getEnvFilePath();
 
-// Return the .env file path
-echo $path;
+// Display each path element of the array in a new line
+foreach ($path as $element) {
+    echo $element . PHP_EOL;
+}
+return $path; // for my insight app , i want to return or nothing
 ?>
+
