ef1f17b0a7e85c3f17c54382350630d68f99f729
mg3994 <126542773+mg3994@users.noreply.github.com>
2024-01-01 19:55:24 +0530

    Create logo_painter.dart

---
+++ flutter_ideas/logo/logo_painter.dart
@@ -0,0 +1,157 @@
+  class LogoPainter extends CustomPainter {
+    @override
+    void paint(Canvas canvas, Size size) {
+      Paint paint = Paint();
+      Path path = Path();
+  
+
+      // Path number 1
+  
+
+      paint.color = Color(0xff3aa9e2);
+      path = Path();
+      path.lineTo(size.width * 1.03, size.height * 0.37);
+      path.cubicTo(size.width * 1.03, size.height * 0.37, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 0.53, size.height * 0.7, size.width * 0.53, size.height * 0.7);
+      path.cubicTo(size.width * 0.53, size.height * 0.7, size.width * 1.03, size.height * 0.37, size.width * 1.03, size.height * 0.37);
+      path.cubicTo(size.width * 1.03, size.height * 0.37, size.width * 1.03, size.height * 0.37, size.width * 1.03, size.height * 0.37);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 2
+  
+
+      paint.color = Color(0xff2d99d4);
+      path = Path();
+      path.lineTo(size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 1.02, size.height * 0.37, size.width * 1.03, size.height * 0.37);
+      path.cubicTo(size.width * 1.03, size.height * 0.37, size.width * 1.52, size.height * 0.7, size.width * 1.52, size.height * 0.7);
+      path.cubicTo(size.width * 1.52, size.height * 0.7, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 3
+  
+
+      paint.color = Color(0xff535b77);
+      path = Path();
+      path.lineTo(size.width * 1.52, size.height * 0.7);
+      path.cubicTo(size.width * 1.52, size.height * 0.7, size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.04);
+      path.cubicTo(size.width * 2.02, size.height * 1.04, size.width * 1.52, size.height * 1.38, size.width * 1.52, size.height * 1.38);
+      path.cubicTo(size.width * 1.52, size.height * 1.38, size.width * 1.52, size.height * 0.7, size.width * 1.52, size.height * 0.7);
+      path.cubicTo(size.width * 1.52, size.height * 0.7, size.width * 1.52, size.height * 0.7, size.width * 1.52, size.height * 0.7);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 4
+  
+
+      paint.color = Color(0xff5f69a2);
+      path = Path();
+      path.lineTo(size.width * 0.53, size.height * 0.7);
+      path.cubicTo(size.width * 0.53, size.height * 0.7, size.width * 0.53, size.height * 1.38, size.width * 0.53, size.height * 1.38);
+      path.cubicTo(size.width * 0.53, size.height * 1.38, size.width * 0.03, size.height * 1.04, size.width * 0.03, size.height * 1.04);
+      path.cubicTo(size.width * 0.03, size.height * 1.04, size.width * 0.53, size.height * 0.7, size.width * 0.53, size.height * 0.7);
+      path.cubicTo(size.width * 0.53, size.height * 0.7, size.width * 0.53, size.height * 0.7, size.width * 0.53, size.height * 0.7);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 5
+  
+
+      paint.color = Color(0xffde4632);
+      path = Path();
+      path.lineTo(size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 1.52, size.height * 1.38, size.width * 1.52, size.height * 1.38);
+      path.cubicTo(size.width * 1.52, size.height * 1.38, size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.72);
+      path.cubicTo(size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 6
+  
+
+      paint.color = Color(0xffed6e2f);
+      path = Path();
+      path.lineTo(size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 0.53, size.height * 1.38, size.width * 0.53, size.height * 1.38);
+      path.cubicTo(size.width * 0.53, size.height * 1.38, size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.72);
+      path.cubicTo(size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      path.cubicTo(size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04, size.width * 1.03, size.height * 1.04);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 7
+  
+
+      paint.color = Color(0xfff09d30);
+      path = Path();
+      path.lineTo(size.width * 2.02, size.height * 1.04);
+      path.cubicTo(size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.48, size.width * 2.02, size.height * 1.69);
+      path.cubicTo(size.width * 2.02, size.height * 1.75, size.width * 2.01, size.height * 1.8, size.width * 1.99, size.height * 1.84);
+      path.cubicTo(size.width * 1.95, size.height * 1.92, size.width * 1.88, size.height * 1.99, size.width * 1.79, size.height * 2.02);
+      path.cubicTo(size.width * 1.72, size.height * 2.04, size.width * 1.64, size.height * 2.05, size.width * 1.55, size.height * 2.02);
+      path.cubicTo(size.width * 1.53, size.height * 2.02, size.width * 1.48, size.height * 1.99, size.width * 1.4, size.height * 1.94);
+      path.cubicTo(size.width * 1.28, size.height * 1.86, size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.72);
+      path.cubicTo(size.width * 1.03, size.height * 1.72, size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.04);
+      path.cubicTo(size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.04);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 8
+  
+
+      paint.color = Color(0xffde4632);
+      path = Path();
+      path.lineTo(size.width * 0.03, size.height * 1.63);
+      path.cubicTo(size.width * 0.03, size.height * 1.43, size.width * 0.03, size.height * 1.04, size.width * 0.03, size.height * 1.04);
+      path.cubicTo(size.width * 0.03, size.height * 1.04, size.width * 1.03, size.height * 1.72, size.width * 1.03, size.height * 1.72);
+      path.cubicTo(size.width * 1.03, size.height * 1.72, size.width * 0.63, size.height * 1.95, size.width * 0.55, size.height * 2);
+      path.cubicTo(size.width / 2, size.height * 2.03, size.width * 0.44, size.height * 2.04, size.width * 0.37, size.height * 2.04);
+      path.cubicTo(size.width * 0.31, size.height * 2.04, size.width / 4, size.height * 2.02, size.width * 0.19, size.height * 1.99);
+      path.cubicTo(size.width * 0.11, size.height * 1.94, size.width * 0.06, size.height * 1.87, size.width * 0.04, size.height * 1.78);
+      path.cubicTo(size.width * 0.02, size.height * 1.74, size.width * 0.03, size.height * 1.68, size.width * 0.03, size.height * 1.63);
+      path.cubicTo(size.width * 0.03, size.height * 1.63, size.width * 0.03, size.height * 1.63, size.width * 0.03, size.height * 1.63);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 9
+  
+
+      paint.color = Color(0xff5f69a2);
+      path = Path();
+      path.lineTo(size.width * 2.02, size.height * 0.45);
+      path.cubicTo(size.width * 2.02, size.height * 0.65, size.width * 2.02, size.height * 1.04, size.width * 2.02, size.height * 1.04);
+      path.cubicTo(size.width * 2.02, size.height * 1.04, size.width * 1.03, size.height * 0.37, size.width * 1.03, size.height * 0.37);
+      path.cubicTo(size.width * 1.03, size.height * 0.37, size.width * 1.43, size.height * 0.13, size.width * 1.5, size.height * 0.09);
+      path.cubicTo(size.width * 1.55, size.height * 0.06, size.width * 1.61, size.height * 0.04, size.width * 1.68, size.height * 0.04);
+      path.cubicTo(size.width * 1.74, size.height * 0.05, size.width * 1.8, size.height * 0.06, size.width * 1.86, size.height * 0.1);
+      path.cubicTo(size.width * 1.93, size.height * 0.14, size.width * 1.98, size.height / 5, size.width * 2.01, size.height * 0.3);
+      path.cubicTo(size.width * 2.02, size.height * 0.35, size.width * 2.02, size.height * 0.4, size.width * 2.02, size.height * 0.45);
+      path.cubicTo(size.width * 2.02, size.height * 0.45, size.width * 2.02, size.height * 0.45, size.width * 2.02, size.height * 0.45);
+      canvas.drawPath(path, paint);
+  
+
+      // Path number 10
+  
+
+      paint.color = Color(0xff269a74);
+      path = Path();
+      path.lineTo(size.width * 1.03, size.height * 0.04);
+      path.cubicTo(size.width * 1.03, size.height * 0.04, size.width * 1.02, size.height * 0.48, size.width * 1.02, size.height * 0.7);
+      path.cubicTo(size.width * 1.02, size.height * 0.75, size.width, size.height * 0.8, size.width, size.height * 0.85);
+      path.cubicTo(size.width * 0.95, size.height * 0.92, size.width * 0.88, size.height, size.width * 0.79, size.height * 1.02);
+      path.cubicTo(size.width * 0.72, size.height * 1.05, size.width * 0.64, size.height * 1.05, size.width * 0.55, size.height * 1.03);
+      path.cubicTo(size.width * 0.53, size.height * 1.02, size.width * 0.48, size.height, size.width * 0.4, size.height * 0.94);
+      path.cubicTo(size.width * 0.28, size.height * 0.86, size.width * 0.03, size.height * 0.72, size.width * 0.03, size.height * 0.72);
+      path.cubicTo(size.width * 0.03, size.height * 0.72, size.width * 1.03, size.height * 0.04, size.width * 1.03, size.height * 0.04);
+      path.cubicTo(size.width * 1.03, size.height * 0.04, size.width * 1.03, size.height * 0.04, size.width * 1.03, size.height * 0.04);
+      canvas.drawPath(path, paint);
+    }
+    @override
+    bool shouldRepaint(CustomPainter oldDelegate) {
+      return true;
+    }
+  }
